c459bcee130e75857dbc95a07be3e4915818114f
RomanLatipov <romanlatipov@gmail.com>
2024-07-15 00:49:47 -0400

    complete quicksort lab

--- javascript/quicksort.js
+++ javascript/quicksort.js
@@ -1,15 +1,35 @@
 function partition(array, low, high) {
-  // type your code in here
+  let i = low - 1; 
+  
+  for (let j = low; j <= high - 1; j++) {
+    if (array[j] < array[high]) {
+      i++;
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp; 
+    } 
+  } 
+  const temp = array[i+1];
+  array[i+1] = array[high];
+  array[high] = temp;
+  return i + 1;
 }
 
 function quicksort(array, low = 0, high = array.length - 1) {
-  // type your code here
+  if (low >= high) 
+    return array;
+  let pivot = partition(array, low, high);
+
+  quicksort(array, low, pivot-1);
+  quicksort(array, pivot+1, high);
+
+  return array;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [1, 2, 3, 4]");
-  console.log(quicksort([3, 2, 1, 4]));
+  console.log(quicksort([3, 2, 6, 4]));
 
   console.log("");
 
